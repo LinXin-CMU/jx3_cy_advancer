f5dc1d9ec77c27e8986b243c378570a3e58bf3f4
霖昕 <1179387241@qq.com>
2022-08-20 22:37:06 +0800

    用于生成复盘图片

---
+++ Scripts/PictureGeneration/pics_equips.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# author: LinXin
+"""
+生成配装图
+"""
+import PIL
---
+++ Scripts/PictureGeneration/pics_setting.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# author: LinXin
+"""
+生成图片的基本设置
+"""
+FONT = r'Sources/UI_Resources/FangZhengYouHei.TTF'
