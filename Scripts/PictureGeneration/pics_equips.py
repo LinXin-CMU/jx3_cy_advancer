486ca6e807f8c0cd91e1b2899ede9f2d1e4b779b
霖昕 <1179387241@qq.com>
2022-08-21 21:47:07 +0800

    2022-08-21 Update

--- Scripts/PictureGeneration/pics_equips.py
+++ Scripts/PictureGeneration/pics_equips.py
@@ -4,13 +4,16 @@
 生成配装图
 """
 import PIL
+from typing import Dict
+
+from CustomClasses.TypeHints import Equip
 
 
 class EquipPictureCreator:
     """
     生成图片格式复盘的类
     """
-    def __init__(self, equip_data: dict):
+    def __init__(self, equip_data: Dict[str, Equip]):
         self._equip_data = equip_data
 
     def run(self):
