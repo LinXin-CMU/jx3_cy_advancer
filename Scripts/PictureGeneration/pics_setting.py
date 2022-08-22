69c5a88450ced600da49fabdda81683ac1157130
霖昕 <1179387241@qq.com>
2022-08-22 09:26:21 +0800

    2022-08-21 Update

--- Scripts/PictureGeneration/pics_setting.py
+++ Scripts/PictureGeneration/pics_setting.py
@@ -4,3 +4,10 @@
 生成图片的基本设置
 """
 FONT = r'Sources/UI_Resources/FangZhengYouHei.TTF'
+
+from collections import namedtuple
+
+position = namedtuple("position", ['x', 'y'])
+size = namedtuple("size", ['width', 'height'])
+rgb = namedtuple("rgb", ['r', 'g', 'b'])
+rgba = namedtuple("rgba", ['r', 'g', 'b', 'a'])
